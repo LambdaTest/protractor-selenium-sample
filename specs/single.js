4d222844b428090e969e957857e44d4a7bc96365
nikhil-lambda <nikhily@lambdatest.com>
2019-04-11 20:13:18 +0530

    mousedown event

--- specs/single.js
+++ specs/single.js
@@ -5,28 +5,9 @@ describe('Add todo Lists', function () {
     it('Add Customer Test', function () {
 
 
-        browser.get('https://lambdatest.github.io/sample-todo-app/');
-
-	browser.driver.findElement(by.name('li1')).then(function (foundElement) {
-                foundElement.click();
-            });
-
-	browser.driver.findElement(by.name('li2')).then(function (foundElement) {
-                foundElement.click();
-            });
-
-	browser.driver.findElement(by.id('sampletodotext')).then(function (foundElement) {
-            foundElement.clear();
-            foundElement.sendKeys("Yey, Let's add it to list");
-            });
-
-	browser.driver.findElement(by.id('addbutton')).then(function (foundElement) {
-                foundElement.click();
-            });
-
-
-		var foo = element(by.xpath('//html/body/div/div/div/ul/li[6]/span'));
-        	expect(foo.getText()).toEqual("Yey, Let's add it to list");
+        browser.get('https://files.epsilon.now.sh/');
+        element  = element(by.id("testing-id"))
+        browser.actions().mouseDown(element.getWebElement()).perform()
 
     });
 
